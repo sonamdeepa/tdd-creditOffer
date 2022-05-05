e7b7d21867c14d1f5bc5962f6ee6ed2e066f7f6f
sonamdeepa <104943608+sonamdeepa@users.noreply.github.com>
2022-05-05 06:19:32 +0100

    Add files via upload

--- src/main/java/com/example/bankapplication/EconomyCreditOffer.java
+++ src/main/java/com/example/bankapplication/EconomyCreditOffer.java
@@ -1,19 +1,22 @@
 package com.example.bankapplication;
 
-public class EconomyCreditOffer extends CreditOffer{
+public class EconomyCreditOffer extends CreditOffer {
 
   public EconomyCreditOffer(String id) {
     super(id);
   }
 
   @Override
-  public boolean addCustomer(Customer customer) {
+  public boolean addCustomer(Customer customer) throws Exception {
+    if (customer.getName().isEmpty()) {
+      throw new Exception("Customer name should not be empty");
+    }
     return customersList.add(customer);
   }
 
   @Override
   public boolean removeCustomer(Customer customer) {
-    if(!customer.isVip()){
+    if (!customer.isVip()) {
       return customersList.remove(customer);
     }
     return false;
